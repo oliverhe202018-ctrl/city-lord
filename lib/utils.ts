65e4d16dafc08eefb812e1fea0cf63105eff2210
oliverhe202018-ctrl <oliverhe202018@gmail.com>
2026-03-10 20:53:47 +0800

    fix(client): use type-only import for Prisma in lib/utils to prevent process.env leak

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,7 +1,7 @@
 import { clsx, type ClassValue } from 'clsx'
 import { twMerge } from 'tailwind-merge'
 
-import { Prisma } from '@prisma/client'
+import type { Prisma } from '@prisma/client'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
