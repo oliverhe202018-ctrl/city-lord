bbac8280ff37f3c4c9523e899646ebe886a8d2e8
Zahra Dewi <oliverhe202018@gmail.com>
2026-06-30 00:30:05 +0800

    fix(ios): remove stray comma in Package.swift target dependencies

--- city-lord-app/ios/App/CapApp-SPM/Package.swift
+++ city-lord-app/ios/App/CapApp-SPM/Package.swift
@@ -1,4 +1,4 @@
-﻿// swift-tools-version: 5.9
+// swift-tools-version: 5.9
 import PackageDescription
 
 // DO NOT MODIFY THIS FILE - managed by Capacitor CLI commands
@@ -13,24 +13,24 @@ let package = Package(
     dependencies: [
         
         .package(url: "https://github.com/groue/GRDB.swift.git", from: "6.29.0"),
-        .package(name: "CapacitorCommunityKeepAwake", path: "..\..\..\node_modules\@capacitor-community\keep-awake"),
-        .package(name: "CapacitorCommunitySafeArea", path: "..\..\..\node_modules\@capacitor-community\safe-area"),
-        .package(name: "CapacitorCommunityTextToSpeech", path: "..\..\..\node_modules\@capacitor-community\text-to-speech"),
-        .package(name: "CapacitorApp", path: "..\..\..\node_modules\@capacitor\app"),
-        .package(name: "CapacitorBrowser", path: "..\..\..\node_modules\@capacitor\browser"),
-        .package(name: "CapacitorCamera", path: "..\..\..\node_modules\@capacitor\camera"),
-        .package(name: "CapacitorClipboard", path: "..\..\..\node_modules\@capacitor\clipboard"),
-        .package(name: "CapacitorDevice", path: "..\..\..\node_modules\@capacitor\device"),
-        .package(name: "CapacitorDialog", path: "..\..\..\node_modules\@capacitor\dialog"),
-        .package(name: "CapacitorGeolocation", path: "..\..\..\node_modules\@capacitor\geolocation"),
-        .package(name: "CapacitorHaptics", path: "..\..\..\node_modules\@capacitor\haptics"),
-        .package(name: "CapacitorKeyboard", path: "..\..\..\node_modules\@capacitor\keyboard"),
-        .package(name: "CapacitorLocalNotifications", path: "..\..\..\node_modules\@capacitor\local-notifications"),
-        .package(name: "CapacitorPreferences", path: "..\..\..\node_modules\@capacitor\preferences"),
-        .package(name: "CapacitorStatusBar", path: "..\..\..\node_modules\@capacitor\status-bar"),
-        .package(name: "CapawesomeCapacitorBackgroundTask", path: "..\..\..\node_modules\@capawesome\capacitor-background-task"),
-        .package(name: "CapgoCapacitorPedometer", path: "..\..\..\node_modules\@capgo\capacitor-pedometer"),
-        .package(name: "CapacitorNativeSettings", path: "..\..\..\node_modules\capacitor-native-settings")
+        .package(name: "CapacitorCommunityKeepAwake", path: "..\\..\\.\\node_modules\\@capacitor-community\\keep-awake"),
+        .package(name: "CapacitorCommunitySafeArea", path: "..\\..\\.\\node_modules\\@capacitor-community\\safe-area"),
+        .package(name: "CapacitorCommunityTextToSpeech", path: "..\\..\\.\\node_modules\\@capacitor-community\\text-to-speech"),
+        .package(name: "CapacitorApp", path: "..\\..\\.\\node_modules\\@capacitor\\app"),
+        .package(name: "CapacitorBrowser", path: "..\\..\\.\\node_modules\\@capacitor\\browser"),
+        .package(name: "CapacitorCamera", path: "..\\..\\.\\node_modules\\@capacitor\\camera"),
+        .package(name: "CapacitorClipboard", path: "..\\..\\.\\node_modules\\@capacitor\\clipboard"),
+        .package(name: "CapacitorDevice", path: "..\\..\\.\\node_modules\\@capacitor\\device"),
+        .package(name: "CapacitorDialog", path: "..\\..\\.\\node_modules\\@capacitor\\dialog"),
+        .package(name: "CapacitorGeolocation", path: "..\\..\\.\\node_modules\\@capacitor\\geolocation"),
+        .package(name: "CapacitorHaptics", path: "..\\..\\.\\node_modules\\@capacitor\\haptics"),
+        .package(name: "CapacitorKeyboard", path: "..\\..\\.\\node_modules\\@capacitor\\keyboard"),
+        .package(name: "CapacitorLocalNotifications", path: "..\\..\\.\\node_modules\\@capacitor\\local-notifications"),
+        .package(name: "CapacitorPreferences", path: "..\\..\\.\\node_modules\\@capacitor\\preferences"),
+        .package(name: "CapacitorStatusBar", path: "..\\..\\.\\node_modules\\@capacitor\\status-bar"),
+        .package(name: "CapawesomeCapacitorBackgroundTask", path: "..\\..\\.\\node_modules\\@capawesome\\capacitor-background-task"),
+        .package(name: "CapgoCapacitorPedometer", path: "..\\..\\.\\node_modules\\@capgo\\capacitor-pedometer"),
+        .package(name: "CapacitorNativeSettings", path: "..\\..\\.\\node_modules\\capacitor-native-settings")
     ],
     targets: [
         .target(
@@ -55,7 +55,7 @@ let package = Package(
                 .product(name: "CapacitorStatusBar", package: "CapacitorStatusBar"),
                 .product(name: "CapawesomeCapacitorBackgroundTask", package: "CapawesomeCapacitorBackgroundTask"),
                 .product(name: "CapgoCapacitorPedometer", package: "CapgoCapacitorPedometer"),
-                ,
+                .product(name: "CapacitorNativeSettings", package: "CapacitorNativeSettings"),
                 .product(name: "GRDB", package: "GRDB.swift")
             ]
         )
